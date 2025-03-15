f6ca2cd995448c09536baec4044582b5d3f7ff6c
Kirushnabalan <kirushnabalan1803@gmail.com>
2025-03-15 12:23:17 +0530

    move to file in app folder

---
+++ src/app/main.jsx
@@ -0,0 +1,12 @@
+// filepath: c:\Users\Kirushnabalan\OneDrive\Desktop\twc\twc app\src\main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
